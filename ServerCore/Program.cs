f6338ee95c3e5de3e0478ff646fd5b75aa2b7b6f
meureuet <gmdgjfr@gmail.com>
2023-09-11 23:43:11 +0900

    interlocked1

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,63 +1,37 @@
 ﻿namespace ServerCore
 {
-    // 메모리 배리어
-    // * 코드 재배치 못하게
-    // * 가시성: 공유 변수 값 동기화
-
-    // 1. full memory barrier: store/load 둘 다 막음 -> Thread.MemoryBarrier()
-    // 2. store memory barrier: store 막음
-    // 3. load memory barrier: load 막음
-
     internal class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int result1 = 0;
-        static int result2 = 0;
-
+        // 경합조건 발생(race condition)
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1; // store y
-
-            Thread.MemoryBarrier(); // 최적화 단계에서 순서 변경 못하도록
-
-            result1 = x;  // load z
+            for(int i = 0; i < 100000; i++) 
+            {
+                number++;
+            }
         }
 
         static void Thread_2()
         {
-            x = 1; // store x
-
-            Thread.MemoryBarrier(); // 최적화 단계에서 순서 변경 못하도록
-
-            result2 = y;  // load y 
+            for (int i = 0; i < 100000; i++)
+            {
+                number--;
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = result1 = result2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                t1.Start();
-                t2.Start();
-                
-                Task.WaitAll(t1, t2);
+            Task.WaitAll(t1, t2);
 
-                // 메모리 배리어 쓰면 아래 조건 성립 안함
-                if(result1 == 0 && result2 == 0)
-                {
-                    break;
-                }
-            }
-
-            Console.WriteLine($"{count}");
+            Console.WriteLine(number);
         }
     }
 }
