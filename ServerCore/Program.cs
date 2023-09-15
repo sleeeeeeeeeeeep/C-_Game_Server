966fde6bf634ccca60dd7428c188f357422c7ec1
meureuet <gmdgjfr@gmail.com>
2023-09-15 22:07:36 +0900

    뮤텍스 사용, 얘도 느림

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,38 +2,19 @@
 
 namespace ServerCore
 {
-    // AutoResetEvent
-    class Lock
-    {
-        // 입장 가능한지(true), 하나가 들어오면 false로 바뀜
-        AutoResetEvent _available = new AutoResetEvent(true);
-
-        // lock
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도
-            // _available.Reset(); // flag -> false로 변경, WaitOne() 에서 실행됨
-        }
-
-        // unlock
-        public void Relase()
-        {
-           _available.Set(); // flag -> true로 변경
-        }
-    }
-
+    // 뮤텍스 사용, 얘도 스핀락보다 느림(커널까지 갔다가 와서)
     internal class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Relase();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -41,9 +22,9 @@ namespace ServerCore
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Relase();
+                _lock.ReleaseMutex();
             }
         }
 
