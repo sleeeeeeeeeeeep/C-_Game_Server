04d8a0680c1b4b490b8f1ccfed777567d1b790f3
meureuet <gmdgjfr@gmail.com>
2023-09-16 14:00:59 +0900

    RW Lock 구현 1 스핀락 기반, 재귀 락 허용하지 않음 헷갈리면 메모장에 비트 적으면서 학습

---
+++ ServerCore/Lock.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    // recursice lock: x
+    // spin lick: 5000번 시도 -> yield
+    internal class Lock
+    {
+        
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // 32비트 구성: [unsued(1)], [WriteThreadId(15)], [ReadCount(16)]
+        int flag;
+
+        public void WriteLock()
+        {
+            // 재귀 락 허용할 때 스레드 id 입력 필요
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK; // id 채움, 다른 곳에 값 있으면 0으로 만듬(& 연산)
+            while(true)
+            {
+                for(int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    // read, write 락 안걸려 있으면(비트 채워지지 않았으면) 지금 스레드 id 넣고 리턴
+                    if (Interlocked.CompareExchange(ref flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        return;
+                    }
+                }
+
+                // 5000번 기다리면 양보
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            // flag에 비어있는 비트 넣음(락 해제)
+            Interlocked.Exchange(ref flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock() 
+        {
+            
+            while(true)
+            {
+                for(int i = 0; i < MAX_SPIN_COUNT; i++) 
+                {
+                    // WriteLock 가진 스레드 없으면, ReadCount 1 증가
+                    int expected = (flag & READ_MASK);
+                    if(Interlocked.CompareExchange(ref flag, expected + 1, expected) == expected)
+                    {
+                        return;
+                    }
+                }
+
+                Thread.Yield();
+            }  
+        }
+
+        public void ReadUnlock()
+        {
+            // ReadCount 1 감소
+            Interlocked.Decrement(ref flag);
+        }
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,52 +4,11 @@ namespace ServerCore
 {
     internal class Program
     {
-        // Monitor 사용
-        static object moniterLock = new object();
-
-        // 스핀락 클래스 사용(너무 오래 락 걸려있으면 쉬다 옴)
-        static SpinLock spinLock = new SpinLock();
-
-        // 느림, 다른 프로그램들 간 락 사용 가능(커널에서 동작하기 때문)
-        static Mutex mutex = new Mutex();
         
-        // RederWriterLock, write 때만 락 걺
-        static ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
-
-        static int getValue(int id)
-        {
-            rwLock.EnterReadLock();
-            // 동작
-            rwLock.ExitReadLock();
-            return 1;
-        }
-
-        static void writeValue(int id, int value)
-        {
-            rwLock.EnterWriteLock();
-            // 동작
-            rwLock.ExitWriteLock();
-        }
 
         static void Main(string[] args)
         {
-            lock (moniterLock)
-            {
-                // 동작
-            }
-
-            bool locked = false;
-            try
-            {
-                spinLock.Enter(ref locked);
-            }
-            finally
-            {
-                if (locked)
-                {
-                    spinLock.Exit();
-                }
-            }
+            
         }
         
     }
