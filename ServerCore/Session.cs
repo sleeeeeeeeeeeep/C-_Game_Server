f479533bb69f7745b901895e4744da082734742b
meureuet <gmdgjfr@gmail.com>
2023-09-20 23:12:02 +0900

    비동기로 send 처리 1차

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -13,20 +13,36 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0;
 
+        object _lock = new object();
+
+        // send할 내역들 큐로 관리
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        bool _pending = false;
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
         public void Start(Socket socket)
         {
             _socket = socket;
             SocketAsyncEventArgs receiveArgs = new SocketAsyncEventArgs();
             receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
-
             receiveArgs.SetBuffer(new byte[1024], 0, 1024);
+
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
             RegisterReceive(receiveArgs);
         }
 
         public void Send(byte[] sendBuffer)
         {
-            _socket.Send(sendBuffer);
+            // 락(멀티스레드 동작)
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuffer);
+                if (_pending == false)
+                {
+                    RegisterSend();
+                }
+            }
         }
 
         public void Disconnect()
@@ -44,6 +60,52 @@ namespace ServerCore
         }
 
         #region 네트워크 통신
+        private void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock(_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        // 큐에 사람있어요
+                        if(_sendQueue.Count > 0)
+                        {
+                            // 처리해
+                            RegisterSend();
+                        }
+                        else
+                        {
+                            _pending = false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"send fail: {e}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+        }
+
+        private void RegisterSend()
+        {
+            // true면 다른 스레드가 RegisterSend() 동작 못하게
+            _pending = true;
+
+            byte[] buffer = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buffer, 0, buffer.Length);
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+            {
+                OnSendCompleted(null, _sendArgs);
+            }
+        }
+
         private void RegisterReceive(SocketAsyncEventArgs args)
         {
             bool pending = _socket.ReceiveAsync(args);
@@ -54,7 +116,7 @@ namespace ServerCore
 
         }
 
-        private void OnReceiveCompleted(object semder, SocketAsyncEventArgs args)
+        private void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
         {
             // 데이터 받기 성공
             if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
@@ -68,12 +130,12 @@ namespace ServerCore
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"fail: {e}");
+                    Console.WriteLine($"recive fail: {e}");
                 }
             }
             else
             {
-
+                Disconnect();
             }
         }
         #endregion
