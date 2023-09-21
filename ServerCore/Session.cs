ea301d5659c7a22aab87f570f3e78c05c84b78bf
meureuet <gmdgjfr@gmail.com>
2023-09-21 22:36:45 +0900

    작업들을 큐로 모았다가 한번에 보냄 send 비동기 처리2

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -15,30 +15,38 @@ namespace ServerCore
 
         object _lock = new object();
 
+        SocketAsyncEventArgs _receiveArgs = new SocketAsyncEventArgs();
+
         // send할 내역들 큐로 관리
         Queue<byte[]> _sendQueue = new Queue<byte[]>();
-        bool _pending = false;
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>(); // 큐에 있었던 모든 데이터
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
 
         public void Start(Socket socket)
         {
             _socket = socket;
-            SocketAsyncEventArgs receiveArgs = new SocketAsyncEventArgs();
-            receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
-            receiveArgs.SetBuffer(new byte[1024], 0, 1024);
+            
+            _receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
+            _receiveArgs.SetBuffer(new byte[1024], 0, 1024);
 
             _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
-            RegisterReceive(receiveArgs);
+            RegisterReceive();
         }
 
+        // send 과정
+        // send 호출 > 큐에 일감 넣음 > 앞에 예약된 애들 없으면 RegisterSend()
+        // -> 큐에 있는 애들 다 빼서 _pendingList에 넣음 -> _pendingList를 _sendArgs.BufferList에 넣음
+        // -> sendAsync로 보냄 -> 보냈으면 OnSendCompleted() -> _pendingList, _sendArgs.BufferList 깔끔하게 지움
+        // -> 보내는 동안에 다른 애가 큐에 집어넣으면 다시 RegisterSend()...
         public void Send(byte[] sendBuffer)
         {
             // 락(멀티스레드 동작)
             lock (_lock)
             {
                 _sendQueue.Enqueue(sendBuffer);
-                if (_pending == false)
+                if (_pendingList.Count == 0)
                 {
                     RegisterSend();
                 }
@@ -60,23 +68,40 @@ namespace ServerCore
         }
 
         #region 네트워크 통신
+        private void RegisterSend()
+        {
+            while(_sendQueue.Count > 0)
+            {
+                byte[] data = _sendQueue.Dequeue();
+                _pendingList.Add(new ArraySegment<byte>(data, 0, data.Length));
+            }
+            _sendArgs.BufferList = _pendingList;
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+            {
+                OnSendCompleted(null, _sendArgs);
+            }
+        }
+
         private void OnSendCompleted(object sender, SocketAsyncEventArgs args)
         {
-            lock(_lock)
+            lock (_lock)
             {
                 if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
                 {
                     try
                     {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();
+
+                        Console.WriteLine($"Transfreed bytes: {_sendArgs.BytesTransferred}");
+
                         // 큐에 사람있어요
-                        if(_sendQueue.Count > 0)
+                        if (_sendQueue.Count > 0)
                         {
                             // 처리해
                             RegisterSend();
-                        }
-                        else
-                        {
-                            _pending = false;
                         }
                     }
                     catch (Exception e)
@@ -91,27 +116,12 @@ namespace ServerCore
             }
         }
 
-        private void RegisterSend()
+        private void RegisterReceive()
         {
-            // true면 다른 스레드가 RegisterSend() 동작 못하게
-            _pending = true;
-
-            byte[] buffer = _sendQueue.Dequeue();
-            _sendArgs.SetBuffer(buffer, 0, buffer.Length);
-
-            bool pending = _socket.SendAsync(_sendArgs);
+            bool pending = _socket.ReceiveAsync(_receiveArgs);
             if (pending == false)
             {
-                OnSendCompleted(null, _sendArgs);
-            }
-        }
-
-        private void RegisterReceive(SocketAsyncEventArgs args)
-        {
-            bool pending = _socket.ReceiveAsync(args);
-            if (pending == false)
-            {
-                OnReceiveCompleted(null, args);
+                OnReceiveCompleted(null, _receiveArgs);
             }
 
         }
@@ -126,7 +136,7 @@ namespace ServerCore
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"client: {recvData}");
 
-                    RegisterReceive(args);
+                    RegisterReceive();
                 }
                 catch (Exception e)
                 {
