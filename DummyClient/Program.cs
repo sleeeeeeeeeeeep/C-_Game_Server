297fa074143f936252a91464c33848a93ddf1acf
meureuet <gmdgjfr@gmail.com>
2023-10-13 23:10:51 +0900

    더미클라이언트 커넥션 수 변경

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -17,7 +17,7 @@ namespace DummyClient
 
             Connector connector = new Connector();
 
-            connector.Connect(endPoint, () => { return SessionManager.Instance.Generate(); }, 10);
+            connector.Connect(endPoint, () => { return SessionManager.Instance.Generate(); }, 100);
 
             while (true)
             {
